cb5cc11ded751a8428ca1e1f349ffcbe9d6aaf1e
camilo <czepedah@uft.edu>
2018-03-27 15:46:16 -0300

    changes on path_matrix_example.py prints of board, the result path and the coord of the path

--- path_example_matrix/path_matrix_example.py
+++ path_example_matrix/path_matrix_example.py
@@ -1,8 +1,9 @@
+#Resolution, path and movements of ghost
+#in PACMAN GAME between to points
 
-'''
-N = 9
-#maze problem
-laberinto = [
+N = 9 #size of matrix
+#board of the game in a matrix of 9x9
+board_game = [
     [0,0,0,0,0,0,0,0,0],
             [0,1,0,1,1,1,0,1,0],
             [0,0,0,0,0,0,0,0,0],
@@ -13,101 +14,98 @@ laberinto = [
             [0,1,0,1,0,1,0,1,0],
             [0,0,0,0,0,0,0,0,0]
 ]
-#MAZE
-print("MATRIZ INICIAL\n")
-for i in range(len(laberinto)):
-    for j in range(len(laberinto[i])):
-        if(laberinto[i][j]==0):
+
+
+
+#Matrix with coord_path
+solution_path = [[0]*N for _ in range(N)]
+coord_path=[]
+
+
+#BACKTRACKING
+def resolve_path(r, c,x_end,y_end):
+    x_end+=1
+    y_end+=1
+   
+    #If we get the result the function ends
+    #the target is x_end,y_end that represent the obj
+    if (r==x_end-1) and (c==y_end-1):
+        solution_path[r][c] = 1;
+        coord_path.append((r,c))
+        print("Ini : 3 , 4 \n")
+        print("Obj :",r,",",c)
+        return True;
+    #check if safe to visit the cells in IF 
+    if r>=0 and c>=0 and r<x_end and c<y_end and solution_path[r][c] == 0 and board_game[r][c] == 0:
+        #if safe to visit then visit the cell
+        solution_path[r][c] = 1
+        #down
+        if resolve_path(r+1, c,x_end-1,y_end-1):
+            coord_path.append((r,c))
+            return True
+        #rigth
+        if resolve_path(r, c+1,x_end-1,y_end-1):
+            coord_path.append((r,c))
+            return True
+        #up
+        if resolve_path(r-1, c,x_end-1,y_end-1):
+            coord_path.append((r,c))
+            return True
+        #left
+        if resolve_path(r, c-1,x_end-1,y_end-1):
+            coord_path.append((r,c))
+            return True
+        #if not posible make a move then the cell is 0
+        #and return False
+        solution_path[r][c] = 0;
+        return False;
+    
+    return 0;
+
+#print the matrix to see the walls and
+#the spaces of the game in this case
+#the spaces is      " . "
+#and the walls are  " # "
+print("BOARD GAME\n")
+for i in range(len(board_game)):
+    for j in range(len(board_game[i])):
+        if(board_game[i][j]==0):
             print(".",end=" ")
         else:
             print("#",end=" ")
     print(" ")
 
 print(" ")
-#se llena la lista de ceros
-solucion = [[0]*N for _ in range(N)]
-coordenadas=[]
-
-
-#BACKTRACKING
-def resolverpath(r, c,x_fin,y_fin):
-    x_fin+=1
-    y_fin+=1
-   
-    #Si se llega al resultado, se termina la funcion
-    #se toma en consideracion x_fin,y_fin como coordenadas target
-    if (r==x_fin-1) and (c==y_fin-1):
-        solucion[r][c] = 1;
-        coordenadas.append((r,c))
-        print("Se llego al punto objetivo R",r,"C",c)
-        return True;
-    #se chequea si es posible acceder a una celda
-    #solution[r][c] == 0 se ve si no se ha visitado
-    #maze[r][c] == 0 se ve si no esta bloqueado
-    if r>=0 and c>=0 and r<x_fin and c<y_fin and solucion[r][c] == 0 and laberinto[r][c] == 0:
-        #if safe to visit then visit the cell
-        solucion[r][c] = 1
-        #hacia abajo
-        if resolverpath(r+1, c,x_fin-1,y_fin-1):
-            coordenadas.append((r,c))
-            return True
-        #hacia la derecha
-        if resolverpath(r, c+1,x_fin-1,y_fin-1):
-            coordenadas.append((r,c))
-            return True
-        #hacia arriba
-        if resolverpath(r-1, c,x_fin-1,y_fin-1):
-            coordenadas.append((r,c))
-            return True
-        #hacia la izquierda
-        if resolverpath(r, c-1,x_fin-1,y_fin-1):
-            coordenadas.append((r,c))
-            return True
-        #backtracking
-        solucion[r][c] = 0;
-        return False;
+print("\nPATH SOLUTION\n")
+#resolve_path recive the coord Ini: 2,4
+#and de the obj the in this case is 5,5
+if(resolve_path(2,4,5,5)):
     
-    return 0;
-if(resolverpath(2,4,4,8)):
-    print("\nPATH RESULTANTE \n")
-    for i in range(len(solucion)):
-        for j in range(len(solucion[i])):
-            if(solucion[i][j]==1):
-                print("*",end=" ")
+    coord_path.reverse()
+    print("\nCOORD OF THE PATH : ",coord_path)
+    for i in range(len(coord_path)):
+        for j in range(2):
+            if(j==0):
+                row=coord_path[i][j]
             else:
+                col=coord_path[i][j]
+        
+        board_game[row][col]=-1
+    print("\n")
+    for i in range(len(board_game)):
+        for j in range(len(board_game[i])):
+            if(board_game[i][j]==-1 ):
+                    
+                print("O",end=" ")
+            if(board_game[i][j]==1):
+                print("#",end=" ")
+            if(board_game[i][j]==0):
                 print(".",end=" ")
             
         print(" ")
-    coordenadas.reverse()
-    print("coordenadas",coordenadas)
+    
+    
 else:
     print ("No solution")
-
-'''
-def solveMaze( Maze , position , x_fin, y_fin ):
-    # returns a list of the paths taken
-    if position == ( x_fin, y_fin  ):
-        return [ ( x_fin, y_fin ) ]
-    x , y = position
-    if x + 1 < x_fin and Maze[x+1][y] == 1:
-        a = solveMaze( Maze , ( x + 1 , y ) , x_fin, y_fin )
-        if a != None:
-            return [ (x , y ) ] + a
-
-    if y + 1 < y_fin and Maze[x][y+1] == 1:
-        b = solveMaze( Maze , (x , y + 1) , x_fin, y_fin )
-        if  b != None:
-            return [ ( x , y ) ] + b
-
-
-Maze = [[ 1 , 0 , 1, 0 , 0],
-        [ 1 , 1 , 0, 1 , 0],
-        [ 0 , 1 , 0, 1 , 0],
-        [ 0 , 1 , 0, 0 , 0],
-        [ 1 , 1 , 1, 1 , 1]
-        ]
-
-print(solveMaze(Maze,(0,0),2,1))
-
 
 
